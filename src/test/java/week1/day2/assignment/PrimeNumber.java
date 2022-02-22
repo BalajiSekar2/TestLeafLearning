06ed29986516465ffcbbfab8787df34353300eae
balajiSekar2 <balajikarthik2@gmail.com>
2022-02-22 23:17:30 +0530

    Assignment 1

---
+++ src/test/java/week1/day2/assignment/PrimeNumber.java
@@ -0,0 +1,25 @@
+package week1.day2.assignment;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		int number = 11;
+		boolean flag = false;
+
+		for(int i=2;i<number;i++) {
+			if(number%i==0) {
+				flag = true;
+				break;
+			}
+		}
+		
+		if(flag==true) {
+			System.out.println("Given number is not a Prime Number:: "+number);
+		}else {
+			System.out.println("Given number is a Prime Number:: "+number);
+		}	
+
+	}
+
+}
